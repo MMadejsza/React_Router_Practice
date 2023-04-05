4f0f52ec95ef96dea290d691d4c650131503efb0
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-04-05 19:26:50 +0100

    ContactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import '../styles/ContactPage.css';
 
-const ContactPage = () => {
-	return (
-		<>
-			<p>ContactPage</p>
-		</>
-	);
-};
+class ContactPage extends React.Component {
+	state = {
+		value: '',
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.setState({
+			value: '',
+		});
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			value: e.target.value,
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<form
+					className='contact'
+					onSubmit={this.handleSubmit}>
+					<h3>Contact As!</h3>
+					<textarea
+						value={this.state.value}
+						onChange={this.handleChange}
+						placeholder='Write here...'
+						cols='30'
+						rows='10'></textarea>
+					<button>Send</button>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default ContactPage;
