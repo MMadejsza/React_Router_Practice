df265d9caab972860eb40abdf3fe1bd50e3a2d76
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-04-05 19:06:52 +0100

    inline css in article (practice)

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 const Article = ({title, author, text}) => {
+	const styles = {
+		marginTop: 40,
+	};
+	// practicing different way of adding css:
 	return (
-		<article>
-			<h3>{title}</h3>
-			<span>{author}</span>
-			<p>{text}</p>
+		<article style={styles}>
+			<h3
+				style={{
+					marginBottom: 3,
+					textTransform: 'uppercase',
+				}}>
+				{title}
+			</h3>
+			<span
+				style={{
+					display: 'block',
+					marginBottom: 10,
+					fontsize: 12,
+				}}>
+				{author}
+			</span>
+			<p
+				style={{
+					fontsize: 15,
+				}}>
+				{text}
+			</p>
 		</article>
 	);
 };
