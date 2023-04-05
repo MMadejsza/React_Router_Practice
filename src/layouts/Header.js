4b1dff3c20a93a490acc7b40eda1249f19a114d5
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-04-05 18:29:03 +0100

    static header images

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,60 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import '../styles/Header.css';
+import img1 from '../images/header1.jpg';
+import img2 from '../images/header2.jpg';
+import img3 from '../images/header3.jpg';
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<Routes>
+			<Route
+				path='/'
+				element={
+					<img
+						src={img1}
+						alt='city'
+					/>
+				}
+			/>
+			<Route
+				path='/products'
+				element={
+					<img
+						src={img2}
+						alt='city'
+					/>
+				}
+			/>
+			<Route
+				path='/contact'
+				element={
+					<img
+						src={img3}
+						alt='city'
+					/>
+				}
+			/>
+			<Route
+				path='/admin'
+				element={
+					<img
+						src={img1}
+						alt='city'
+					/>
+				}
+			/>
+			<Route
+				path='*'
+				element={
+					<img
+						src={img1}
+						alt='city'
+					/>
+				}
+			/>
+		</Routes>
+	);
 };
 
 export default Header;
