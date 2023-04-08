f56cf0933ae5614ffed90e250b2cf0a262dc7083
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-04-08 14:12:32 +0100

    update for gh pages

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -9,7 +9,7 @@ import Footer from './Footer';
 class App extends Component {
 	render() {
 		return (
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<div className='App'>
 					<header>{<Header />}</header>
 					<main>
