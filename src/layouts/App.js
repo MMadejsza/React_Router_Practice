3c67cac38d67a1e3970a61a196351b330c399ca9
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-04-08 13:54:38 +0100

    checking scss + gh-pages

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import '../styles/App.css';
+import '../styles/App.scss';
 import {BrowserRouter as Router, Navlink, Route, Routes} from 'react-router-dom';
 import Header from './Header';
 import Navigation from './Navigation';
